32ab528fe0a25649979f20edb9bf93693da167a3
peterkeep <peter.s.keep@gmail.com>
2019-11-09 20:42:18 -0600

    Play/Pause, print from Python

--- gpmdp-nowplaying.py
+++ gpmdp-nowplaying.py
@@ -1,15 +1,25 @@
+#!/usr/bin/env python
+
 import json
 
-with open('/home/[USER]/.config/Google Play Music Desktop Player/json_store/playback.json') as json_file:
+with open('/home/peter/.config/Google Play Music Desktop Player/json_store/playback.json') as json_file:
     data = json.load(json_file)
     title =  data['song']['title']
     artist = data['song']['artist']
     album = data['song']['album']
 
-np = title + ' - ' + artist + ' (' + album + ')'
 
-print(np)
 
-file = open('/home/[USER]/.nowplaying', 'w')
-file.write(np)
-file.close()
+t = str(title)
+a = str(artist)
+b = str(album)
+
+
+if t == 'None':
+    np = str('Nothing currently playing.')
+elif data['playing']:
+    np = str('Now playing:   ' + t + ' - ' + a + ' (' + b + ')')
+else:
+    np = str('Paused:   ' + t + ' - ' + a + ' (' + b + ')')
+
+print np
