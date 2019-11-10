5173faff05d786aede58546c89800fadaa045730
peterkeep <peter.s.keep@gmail.com>
2019-11-09 20:51:40 -0600

    [USER]

--- gpmdp-nowplaying.py
+++ gpmdp-nowplaying.py
@@ -2,18 +2,15 @@
 
 import json
 
-with open('/home/peter/.config/Google Play Music Desktop Player/json_store/playback.json') as json_file:
+with open('/home/[USER]/.config/Google Play Music Desktop Player/json_store/playback.json') as json_file:
     data = json.load(json_file)
     title =  data['song']['title']
     artist = data['song']['artist']
     album = data['song']['album']
 
-
-
 t = str(title)
 a = str(artist)
 b = str(album)
-
 
 if t == 'None':
     np = str(' ')
